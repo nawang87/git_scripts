e2032957f4a1d75f08a442380aa699ee940978b3
Nawagn Chegen Lama <NawagnChegen.Lama@logpoint.com>
2018-05-02 08:01:45 +0545

    Removed the port from url

--- git_url_updater.sh
+++ git_url_updater.sh
@@ -21,16 +21,16 @@ for i in `cat $TMP_REPO_DIR_LIST | sed -e 's/.git$//'`;
 			continue
 		fi
 
-		#origin	ssh://git@new.url.local:7999/mc/config.git (fetch)
-		#fetch mc from above line
+		#origin	ssh://git@new.url.local/mc/config.git (fetch)
+		#fetch mc from above line which
 		project_tag=$(sudo git remote -v | grep fetch | awk -F "/" '{print $4}')
 
-		#origin	ssh://git@new.url.local:7999/mc/config.git (fetch)
+		#origin	ssh://git@new.url.local/mc/config.git (fetch)
 		#fetch config.git from above line
 		rep_git_name=$(sudo git remote -v | grep fetch | awk -F "/" '{print $5}' | awk '{print $1}')
 
 		#Setting new url
-		sudo git remote set-url origin ssh://git@"$NEW_URL":7999/${project_tag}/${rep_git_name};
+		sudo git remote set-url origin ssh://git@"$NEW_URL"/${project_tag}/${rep_git_name};
 
 		echo "New git URL:"
 		sudo git remote -v
